3f2f55a51ad3415c12d63036a486ba38d4541afe
tsiory17 <tsiory.r17@gmail.com>
2025-10-06 21:17:38 -0400

    new commit

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,4 +1,30 @@
+"use server";
 // flow
 // Enter name + email → Check if exists → Send OTP → Verify OTP → Create user → Authenticate
 //
 // Enter email → Check if exists → Send OTP → Verify OTP → Authenticate
+
+import { createAdminClient } from "@/lib/appwrite";
+import { appwriteConfig } from "@/lib/appwrite/config";
+import { Query } from "node-appwrite";
+
+const getUserByEmail = async (email: string) => {
+  const { databases } = await createAdminClient();
+  const result = await databases.listRows(
+    appwriteConfig.databaseId,
+    appwriteConfig.usersTable,
+    [Query.equal("email", email)],
+  );
+
+  return result.total > 0 ? result.Rows[0] : null;
+};
+
+const createAccount = async ({
+  fullName,
+  email,
+}: {
+  fullName: string;
+  email: string;
+}) => {
+  const existingAccount = await getUserByEmail(email);
+};
--- lib/appwrite/index.ts
+++ lib/appwrite/index.ts
@@ -1,3 +1,4 @@
+"use server";
 import { Client, Account, Databases, Storage, Avatars } from "node-appwrite";
 import { appwriteConfig } from "@/lib/appwrite/config";
 import { cookies } from "next/headers";
