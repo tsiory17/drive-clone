a2a6c2be070ab6f2ba958f83cff3ddf6145c231e
tsiory17 <tsiory.r17@gmail.com>
2025-10-14 20:12:28 -0400

    continuing implementation of otp

--- components/OTPModal.tsx
+++ components/OTPModal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   AlertDialog,
   AlertDialogAction,
@@ -10,23 +10,103 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
-export default function OTPModal() {
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
+import Image from "next/image";
+import { buttonVariants } from "@/components/ui/button";
+import { verifySecret } from "@/lib/actions/user.actions";
+import { useRouter } from "next/router";
+
+export default function OTPModal({
+  accountId,
+  email,
+}: {
+  accountId: string;
+  email: string;
+}) {
+  // const [value, setValue] = React.useState("");
+  const router = useRouter();
+  const [isOpen, setIsOpen] = useState(true);
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    try {
+      // call api to verify otp
+      const sessionId = await verifySecret({ accountId, password });
+
+      if (sessionId) {
+      }
+    } catch (e) {
+      console.log("Failed to verify OTP", e);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleResendOTP = async () => {
+    console.log("Resend OTP");
+  };
+  // call api to resent the otp
   return (
     <div>
-      <AlertDialog>
-        <AlertDialogTrigger>Open</AlertDialogTrigger>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-            <AlertDialogDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
+      <AlertDialog open={isOpen} onOpenChange={setIsOpen}>
+        <AlertDialogContent className="shad-alert-dialog">
+          <AlertDialogHeader className="justify center relative flex">
+            <AlertDialogTitle className="h2 text-center">
+              Enter your OTP
+            </AlertDialogTitle>
+            <AlertDialogDescription className="subtitle-2 text-center">
+              we have sent and email to{" "}
+              <span className="pl-1 text-brand">{email}</span>
             </AlertDialogDescription>
+
+            <div className="flex flex-col items-center justify-center space-y-8">
+              <InputOTP maxLength={6} value={password} onChange={setPassword}>
+                <InputOTPGroup className="shad-otp">
+                  <InputOTPSlot index={0} />
+                  <InputOTPSlot index={1} />
+                  <InputOTPSlot index={2} />
+                  <InputOTPSlot index={3} />
+                  <InputOTPSlot index={4} />
+                  <InputOTPSlot index={5} />
+                </InputOTPGroup>
+              </InputOTP>
+            </div>
           </AlertDialogHeader>
-          <AlertDialogFooter>
+
+          <AlertDialogFooter className="flex w-full flex-col items-center gap-2 sm:flex-row sm:justify-center">
             <AlertDialogCancel>Cancel</AlertDialogCancel>
-            <AlertDialogAction>Continue</AlertDialogAction>
+            <AlertDialogAction
+              onClick={handleSubmit}
+              className="shad-submit-btn h12"
+              type="button"
+            >
+              Submit
+              {isLoading && (
+                <Image
+                  src="/assets/icons/loader.svg"
+                  alt="loader"
+                  width={24}
+                  height={24}
+                  priority={true}
+                />
+              )}
+            </AlertDialogAction>
           </AlertDialogFooter>
+          <div className="subtitle-2 flex items-center justify-center">
+            Did not get the code?{" "}
+            <button onClick={handleResendOTP} className="pl-1 text-brand">
+              Resend code
+            </button>
+          </div>
         </AlertDialogContent>
       </AlertDialog>
     </div>
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -8,6 +8,7 @@ import { createAdminClient } from "@/lib/appwrite/index";
 import { appwriteConfig } from "@/lib/appwrite/config";
 import { Query, ID } from "node-appwrite";
 import { parseStringify } from "../utils";
+import { cookies } from "next/headers";
 
 const getUserByEmail = async (email: string) => {
   const { databases } = await createAdminClient();
@@ -20,22 +21,21 @@ const getUserByEmail = async (email: string) => {
   return result.total > 0 ? result.documents[0] : null;
 };
 
-const handleError = (error: unknown , message: string) => {
-  console.log(error,message);
+const handleError = (error: unknown, message: string) => {
+  console.log(error, message);
   throw error;
-}
+};
 
-const sendEmailOTP = async ({email}:{email: string}) => {
-  const {account } = await createAdminClient();
+const sendEmailOTP = async ({ email }: { email: string }) => {
+  const { account } = await createAdminClient();
 
   try {
-    const session = await account.createEmailToken(ID.unique(),email);
+    const session = await account.createEmailToken(ID.unique(), email);
     return session.userId;
-  }catch (e) {
-      handleError(e,"Failed to send OTP")
+  } catch (e) {
+    handleError(e, "Failed to send OTP");
   }
-
-}
+};
 
 export const createAccount = async ({
   fullName,
@@ -46,16 +46,16 @@ export const createAccount = async ({
 }) => {
   const existingUser = await getUserByEmail(email);
 
-  const accountId = await sendEmailOTP({email});
-  if(!accountId) throw new Error ("Failed to send an OTP");
+  const accountId = await sendEmailOTP({ email });
+  if (!accountId) throw new Error("Failed to send an OTP");
 
-  if(!existingUser) {
-    const { databases } =  await createAdminClient();
+  if (!existingUser) {
+    const { databases } = await createAdminClient();
 
     await databases.createDocument(
       appwriteConfig.databaseId,
       appwriteConfig.usersTable,
-      ID.unique() ,
+      ID.unique(),
       {
         fullName,
         email,
@@ -64,5 +64,29 @@ export const createAccount = async ({
       },
     );
   }
-  return parseStringify({ accountId })
+  return parseStringify({ accountId });
+};
+
+export const verifySecret = async ({
+  accountId,
+  password,
+}: {
+  accountId: string;
+  password: string;
+}) => {
+  try {
+    const { account } = await createAdminClient();
+    const session = await account.createSession(accountId, password);
+
+    (await cookies()).set(`appwrite-session`, session.secret, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+      secure: true,
+    });
+
+    return parseStringify({ sessionId: session.$id });
+  } catch (e) {
+    handleError(e, "Failed to verify OTP");
+  }
 };
